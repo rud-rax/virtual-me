95f98e69500d98bb6c38b30e3d4f2a6d5b2c9af1
Rudraksh Naik <rudrakshnaik8990@gmail.com>
2024-05-17 15:38:22 +0530

    Updated code to chat in terminal.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,13 @@ from src.langflow_request import run_flow
 FLOW_ID = "e41b3d64-a2f4-4907-aa87-9d14c4a178f4"
 
 
-message = "What kind of Python modules have you used ?"
-bot_response = run_flow(message=message, flow_id=FLOW_ID)
-print(bot_response['outputs'][0]['outputs'][0]['results']['result'])
+# message = "What kind of Python modules have you used ?"
+while True : 
+        
+    message = input("Question : ")
+
+    if message.rstrip() == "EXIT" : break
+
+    bot_response = run_flow(message=message, flow_id=FLOW_ID)
+    print("Answer : " , bot_response['outputs'][0]['outputs'][0]['results']['result'])
 
